cb00f1d0dd9dd7ab342f944e75b5458db6dff94c
Vlasov Danil <asingleextrasecond@gmail.com>
2025-12-08 04:16:22 +0800

    Bugy version of dynamic char array, today i'm gonna kms.

--- theLongestString.c
+++ theLongestString.c
@@ -2,14 +2,19 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+#define MINSPACE 2                                                                                  // Я думаю, что 2 или 3 достаточно, так как на конце массива чаров будет \n и \0,
+                                                                                                    // но \n входит в длину строки, поэтому он считается. Остается одина свободная ячейка.
+#define DELTACAPACITY 20                                                                            // Не знаю сколько добавлять ячеек памяти к новому массиву. Не хочу слишком много,
+                                                                                                    // пустого пространства, а слишком мало, чтобы не занимать такты.
+
 //  Under this comment are some kinda templates for functions. In C we can't use templates like in C++, and I don't want to copy-paste functions with diffence in data type in params. 
 
 #define MAKE_DYNAMIC_ARRAY(DATA_TYPE)                                                               \
-    DATA_TYPE* makeDynamicArray_##DATA_TYPE(int capacity, int typeSize){            \
-        DATA_TYPE* ptr = (DATA_TYPE *)malloc(capacity * typeSize);                                             \
+    DATA_TYPE* makeDynamicArray_##DATA_TYPE(int capacity, int typeSize){                            \
+        DATA_TYPE* ptr = (DATA_TYPE *)calloc(capacity, typeSize);                                   \
         if(ptr == NULL){                                                                            \
             printf("Failed to allocate memory for dynamic array");                                  \
-            return NULL;                                                                            \
+            exit(0);                                                                                \
         }                                                                                           \
         else                                                                                        \
             return ptr;                                                                             \
@@ -19,10 +24,10 @@ MAKE_DYNAMIC_ARRAY(char)
 
 #define CHANGE_CAPACITY_DYNAMIC_ARRAY(DATA_TYPE)                                                    \
     DATA_TYPE* changeCapacityDynamicArray_##DATA_TYPE(DATA_TYPE *ptr, int capacity, int typeSize){  \
-        ptr = (DATA_TYPE *)realloc(ptr, capacity * typeSize);                                            \
+        ptr = (DATA_TYPE *)realloc(ptr, capacity * typeSize);                                       \
         if(ptr == NULL){                                                                            \
             printf("Failed to allocate memory for dynamic array");                                  \
-            return NULL;                                                                            \
+            exit(0);                                                                                \
         }                                                                                           \
         else                                                                                        \
             return ptr;                                                                             \
@@ -30,7 +35,15 @@ MAKE_DYNAMIC_ARRAY(char)
 CHANGE_CAPACITY_DYNAMIC_ARRAY(int)
 CHANGE_CAPACITY_DYNAMIC_ARRAY(char)
 
-
+char* checkStringCapacity (char string[], int *ptrCapacity, int length){
+    int capacity = *ptrCapacity;
+    if(length + MINSPACE >= capacity){
+        capacity += DELTACAPACITY;
+        *ptrCapacity = capacity;
+        string = changeCapacityDynamicArray_char(string, capacity, sizeof(char));
+    }
+    return string;
+}
 
 void outputString(char inputString[]){
     printf("%s", inputString);
@@ -42,30 +55,45 @@ void copyString(char inputString[], char copyString[]){
         ++i;
 }
 
-int readLine(char string[], int limit){
-    int c, length = 0;
-    while((c = getchar()) != EOF && length < limit - 1){
+int readLine(char **ptrString, int *ptrCapacity){
+    int c, length = 0, capacity = *ptrCapacity;
+    char *string = *ptrString;
+    while((c = getchar()) != EOF){
+        string = checkStringCapacity(string, ptrCapacity, length);
         string[length] = c;
         length++;
-        if(c == '\n')
+        if(c == '\n'){
+            if(length + MINSPACE >= capacity){
+                capacity += MINSPACE;
+                *ptrCapacity = capacity;
+                string = changeCapacityDynamicArray_char(string, capacity, sizeof(char));
+            }
             break;
+        }
     }
+    string[length] = '\0';
+    *ptrString = string;
     return length;
 }
 
 int main(){
     int length = 0, maxlength = 0, capacity = 10;
-    int typeSize = sizeof(int);                     // I'm using int for char, because EOF not in range of char. 
-    int *string, *maxString;                        // Same shit here. Int pointers for dynamic arrays of symbols, probably like a string, but not sure.
+    //int *ptrCapacity = &capacity;                      
+    char *string = NULL, *maxString = NULL;
+    string = makeDynamicArray_char(capacity, sizeof(char));       
+    maxString = makeDynamicArray_char(capacity, sizeof(char));
+    // Передаю адрес capacity, чтобы  динамически его поменять в функции readline, если длина строки будет больше неообходимого.
+    while((length = readLine(&string, &capacity)) > 0){
+        if(length > maxlength){
+            maxlength = length;
+            maxString = changeCapacityDynamicArray_char(maxString, capacity, sizeof(char));
+            copyString(string, maxString);
+        }
+    }
 
-    
-
-    //while((length = readLine(string, MAXLENGTH)) > 0){
-    //    if(length > maxlength){
-    //        maxlength = length;
-    //        copyString(string, maxString);
-    //    }
-    //}
-    //printf("\nThe longest with \\n: %d characters\n", maxlength);
-    //outputString(maxString);
+    printf("\nThe longest with \\n: %d characters\n", maxlength);
+    outputString(maxString);
+    printf("Capacity is equal to: %d", capacity);
+    free(string);
+    free(maxString);
 }
