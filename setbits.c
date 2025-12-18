cc7e9952ef15286de47569a7d92d383f24f8bbaa
Vlasov Danil <asingleextrasecond@gmail.com>
2025-12-18 15:29:23 +0800

    Made a function setbits which copy last n bits of value y and put this n bits into value x from p position

---
+++ setbits.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int setbits(int x, int p, int n, int y){
+    return (((~0 << (p+1)) | ~(~0 << (p+1-n))) & x) | (~(~0 << n) & y) << (p+1-n);
+}
+
+void printBinary(unsigned int n) {
+    for (int i = (sizeof(unsigned int) * 8) - 1; i >= 0; i--) {
+        putchar((n & (1 << i)) ? '1' : '0');
+    }
+    printf("\n");
+}
+
+int main(){
+    unsigned int x, p, n, y;
+    scanf("%d %d %d %d", &x, &p, &n, &y);
+    printBinary(x);
+    printBinary(y);
+    printBinary(setbits(x, p, n, y));
+    x = setbits(x, p, n, y);
+    printf("%d \n", x);
+}
