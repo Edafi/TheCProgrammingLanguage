0b30ab5d3e518472cb18ab6fda20798f62acbd7d
Vlasov Danil <asingleextrasecond@gmail.com>
2026-01-12 02:41:27 +0800

    strcpy and strcat are selfmaded copies of an original C standard library functions

---
+++ strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define TESTSIZE 1000
+
+void strcat_SELFMADE (char *string, char *copy){
+    for(; *string; string++)
+        ;
+    if(*string == '\0')
+        while((*string++ = *copy++) != '\0')
+            ;
+    else
+        printf("Something went wrong. strcat_SELFMADE");
+}
+
+int main(){
+    char originalString[TESTSIZE] = "Hello Robbert.";
+    char copyString[] = " How are you today?\n";
+    printf("First string is - %s\n", originalString);
+    printf("Second string is -%s\n", copyString);
+
+    strcat_SELFMADE(originalString, copyString);
+    printf("Merged string is - %s\n", originalString);
+}
---
+++ strcpy.c
@@ -0,0 +1,14 @@
+void strcpy_SELFMADE(char *from, char *to){
+    while((*to++ = *from++) != 0)
+        ;
+}
+
+int strcmp_SELFMADE (char *first, char *second){
+//    while (*first || *second){
+//        return ((*first < *second)? -1 : ((*first == *second)? 0 : 1));
+//    }
+    for( ; *first == *second; first++, second++)
+        if(*first == '\0')
+            return 0;
+    return *first - *second;
+}
