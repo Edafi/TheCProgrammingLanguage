66dfdeb4405099d5f12816b801eb0da60c6c12cf
Vlasov Danil <asingleextrasecond@gmail.com>
2026-01-21 00:06:11 +0800

    find function with aruments (-x and -n)

--- find.c
+++ find.c
@@ -7,14 +7,34 @@
 
 int main(int argc, char *argv[]){
     char *line = calloc(MAXLINE, sizeof(char));
-    int found = 0;
+    long lineno = 0;
+    int c, except = 0, number = 0, found = 0;
     size_t capacity = MAXLINE; 
 
-    if(argc != 2)
-        printf("Invalid arguments. find <pattern>\n");
+    while (--argc > 0 && (*++argv)[0] == '-')
+        while (c = *++argv[0])
+            switch (c) {
+                case 'x':
+                    except = 1;
+                    break;
+                case 'n':
+                    number = 1;
+                    break;
+                default:
+                    printf("find: illegal option %c\n", c);
+                    argc = 0;
+                    found = -1;
+                    break;
+            }
+
+    if(argc != 1)
+        printf("Invalid arguments. find -x -n <pattern>\n");
     else{
         while(getline(&line, &capacity, stdin) > 0){
-            if(strstr(line, argv[1]) != NULL){
+            lineno++;
+            if((strstr(line, *argv) != NULL) != except){
+                if (number)
+                    printf("%ld:", lineno);
                 printf("%s", line);
                 found++;
             }
