b7b06a069accd872355c3dfcc969d5ff39240987
Vlasov Danil <asingleextrasecond@gmail.com>
2026-03-27 19:06:25 +0800

    Hashtable is working, implemented printing hashtable

--- table_search.c
+++ table_search.c
@@ -5,7 +5,7 @@
 
 struct nlist{										// This is something like map where is 
 													// key (name) and value.
-    struct nlist *nextList;							// Next element of the list
+    struct nlist *nextElement;						// Next element of the list
  	char *name;
     char *value;
 };
@@ -14,6 +14,7 @@ static struct nlist *hashTable[TABLESIZE];      	// Array of pointers to nlist s
 
 #define HASHSEED 42
 // A K&R Hash function
+
 u_short hash(char *string){
 	u_short hashValue;
 	
@@ -25,7 +26,7 @@ u_short hash(char *string){
 struct nlist *lookup(char *string){
 	struct nlist *pointerNext;
 	
-	for (pointerNext = hashTable[hash(string)]; pointerNext != NULL; pointerNext = pointerNext -> nextList)
+	for (pointerNext = hashTable[hash(string)]; pointerNext != NULL; pointerNext = pointerNext -> nextElement)
 		if (strcmp(string, pointerNext -> name) == 0)
 			return pointerNext;						// We found element from #define
 	return NULL;									// No occurrence have been found
@@ -38,10 +39,10 @@ struct nlist *install(char *name, char *value){
 	if ((pointerNext = lookup(name)) == NULL){		// We didn't find the name in hashTable
 		pointerNext = (struct nlist *) malloc(sizeof(struct nlist *));	// Init the new element
 		if (pointerNext == NULL || (pointerNext -> name = strdup(name)) == NULL)	
-			// If created element is NULL or stedup can't allocate space for name or value we return NULL
+													// If created element is NULL or stedup can't allocate space for name or value we return NULL
 			return NULL;
 		hashValue = hash(name);						// Creating new hash for index
-		pointerNext -> nextList = hashTable[hashValue];	// Moving the old one element with the same hash to the next position
+		pointerNext -> nextElement = hashTable[hashValue];	// Moving the old one element with the same hash to the next position
 		hashTable[hashValue] = pointerNext;			// Adding  new element to the table
 	}
 	else											// We found the name in hashTable
@@ -51,6 +52,19 @@ struct nlist *install(char *name, char *value){
 	return pointerNext;
 }
 
+void print_hashtable(void){
+	struct nlist *element;
+	int index = 0;
+	for (element = hashTable[index]; index != TABLESIZE; element = hashTable[++index]){
+		while (element != NULL){
+			printf ("index: %d; name: %s; value: %s\n", index, element -> name, element -> value);
+			element = element -> nextElement;
+		}
+	}
+}
+
 int main(){
-    printf("short size is %ld", sizeof(int));
+	struct nlist *pi = install("pi", "3.141592");
+	struct nlist *e = install("e", "2.1828");
+  	 print_hashtable();	
 }
